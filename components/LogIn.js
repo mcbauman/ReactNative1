0eb9cc593b34e79c3cf790f6abdb420f1988a7e3
mcbauman <m.c.baumann@icloud.com>
2022-09-07 12:52:34 +0200

    Login working

--- components/LogIn.js
+++ components/LogIn.js
@@ -1,26 +1,39 @@
-import React from "react";
-import { Button,Form,Input } from "react-native"
+import React, { useEffect, useState } from "react";
+import { Button,Text,TextInput, View } from "react-native"
 import {REACT_APP_BE_SERVER} from "@env"
-import axios from "axios"
+import axios from "axios";
+import { BlurView } from "expo-blur";
 
 export default function LogIn(props){
+    const [userName,setUserName]=useState("UserName");
+    const [password, setPassword]=useState("");
+
     function LogIn(){
-        axios.post(`${REACT_APP_BE_SERVER}/Login`,{name:"matthias",password:"aA@123456"})
+        axios.post(`${REACT_APP_BE_SERVER}/Login`,{name:userName,password:password})
         .then((res)=>{
-            console.log(res.data);
+            console.log("Res.Data",res.data);
             props.setUser(res.data);
+            console.log(userName);
         })
     }
 
     return (
-        // <Form ref="form">
-        //     <Input name="name" label="Name"></Input>
-        //     <Input name="password" label ="Password"></Input>
+        <View>
+            <Text>{userName}</Text>
+            <BlurView intensity={10} style={{backgroundColor:'rgba(0, 0, 0, 0.3)'}}>
+                <TextInput onChangeText={text=>setUserName(text)} 
+                placeholder="Name"
+                name="name" label="Name"></TextInput>
+            </BlurView>
+            <BlurView intensity={10} style={{backgroundColor:'rgba(0, 0, 0, 0.3)'}}>
+                <TextInput onChangeText={text=>setPassword(text)}
+                placeholder="*****"
+                name="password" label ="Password"></TextInput>
+            </BlurView>
             <Button
             onPress = {LogIn}
             title = "Log In"
-            color = "white"
-         />
-        // </Form>
+            color = "white"/>
+        </View>
     )
 }
